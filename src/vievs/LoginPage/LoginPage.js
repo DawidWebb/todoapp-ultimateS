10664ab73da210b103269ac0a8715bac169c378d
DawidWebb <tomasz.matras@developerweb.pl>
2021-09-28 12:15:05 +0200

    clear: clear console.logs, not use dependencies.

--- src/vievs/LoginPage/LoginPage.js
+++ src/vievs/LoginPage/LoginPage.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { addLogin } from "../../data/actions";
 import { Link } from "react-router-dom";
 import { Button } from "../../components";
